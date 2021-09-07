9ddbea41eee4f9c0fb80bf29a46c56e1fcc437e6
Candice <candice.vernacchio@gmail.com>
2021-09-07 17:45:28 -0600

    commit tree d174a1314587a0c2279d802e831a066093c50bdd

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = (num1, num2) => {
+    num1 = 2000
+    num2 = 100
+    return num1 / num2
+}
+const square = (x) => {
+    x = 2
+    return x * x
+}
+const add = (a, b) => {
+    a = 3
+    b = 4
+    return a + b
+}
